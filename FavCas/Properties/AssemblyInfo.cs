12e7e18752d88527dd82f13e16fa3fe1e9ff0387
mayth <chimeaquas@hotmail.com>
2012-01-15 13:11:49 +0900

    Add copyright info, and other.

--- FavCas/Properties/AssemblyInfo.cs
+++ FavCas/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("FavCas")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Best Friend of your Favs")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("mayth")]
 [assembly: AssemblyProduct("FavCas")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright ©mayth  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
